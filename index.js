d5dae433e789c1110f8e02a0c63b3939b37c2b10
Suhail Abdi <suhailabdi857@gmail.com>
2025-06-12 10:41:31 +0300

    added solution for number one

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 /* Add you solutions here! */
+Array.from({length:10},() =>{
+    const li=document.createElement("li");
+    const img=document.createElement("img");
+    img.src="https://picsum.photos/200" ;
+    li.append(img);
+    document.querySelector("ul").append(li);
+
+})
